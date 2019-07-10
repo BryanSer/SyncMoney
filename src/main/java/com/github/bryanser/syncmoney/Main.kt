2de7c1c8fcfcbf5d450dd4320eeb6652690dbf95
BryanSer <bryan_lzh@njust.edu.cn>
2019-07-10 09:59:36 +0800

    init

---
+++ src/main/java/com/github/bryanser/syncmoney/Main.kt
@@ -0,0 +1,15 @@
+package com.github.bryanser.syncmoney
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
